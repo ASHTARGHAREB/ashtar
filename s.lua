4644e5712e1caeaaa8ab75c59211c59e78931937
ASHTARGHAREB <37347909+ASHTARGHAREB@users.noreply.github.com>
2018-03-14 04:46:49 +0200

    Update s.lua

--- s.lua
+++ s.lua
@@ -1,9 +1,8 @@
 --[[
-BY : TshAkETEAM
-Channel Files : https://t.me/tshakeFiles
+BY : AshtaRGhareB
 ]]
 
-local function keko_tshake(data)
+local function AshtaR_GhareB(data)
     JSON = (loadfile  "./libs/dkjson.lua")()
     local msg = data.message_
     URL = require('socket.url')
@@ -12,7 +11,7 @@ local function keko_tshake(data)
     database = Redis.connect('127.0.0.1', 6379)
     sudos = dofile('sudo.lua')
     HTTP = require("socket.http")
-    bot_id_keko = {string.match(token, "^(%d+)(:)(.*)")}
+    bot_id_AshtaR = {string.match(token, "^(%d+)(:)(.*)")}
     bot_id = tonumber(bot_id_keko[1])
     local function send(chat_id, reply_to_message_id, disable_notification, text, disable_web_page_preview, parse_mode)
         local TextParseMode = {ID = "TextParseModeMarkdown"}
@@ -50,8 +49,8 @@ local function keko_tshake(data)
             var = true
             end
               end
-              local keko_add_sudo = redis:get('sudoo'..user_id..''..bot_id)
-              if keko_add_sudo then
+              local AshtaR_add_sudo = redis:get('sudoo'..user_id..''..bot_id)
+              if AshtaR_add_sudo then
               var = true
               end
             return var
@@ -59,33 +58,32 @@ end
 if text then 
 if text == 'تعطيل الاختصار' then 
 send(msg.chat_id_, msg.id_, 1, "تم تعطيل اختصار الروابط في المجموعه 🖲┇", 1, 'html')
-database:set("keko:short"..bot_id..msg.chat_id_,'keko')
+database:set("AshtaR:short"..bot_id..msg.chat_id_,'AshtaR')
 end
 if text == 'تفعيل الاختصار' then 
 send(msg.chat_id_, msg.id_, 1, "تم تفعيل اختصار الروابط في المجموعه 🖲┇", 1, 'html')
-database:del("keko:short"..bot_id..msg.chat_id_)
+database:del("AshtaR:short"..bot_id..msg.chat_id_)
 end
 
-local keko = database:get("keko:short"..bot_id..msg.chat_id_)
-if not keko then 
+local AshtaR = database:get("AshtaR:short"..bot_id..msg.chat_id_)
+if not AshtaR then 
 local text = msg.content_.text_:gsub('short','اختصار')
 if text:match("^(اختصار) (.*)$") then 
-local keko2 = {string.match(text, "^(اختصار) (.*)$")}
-local req = HTTP.request('http://caut-link.tk/api/?api=a591ca34f771237a385fd9ebadc13e0ec48490f7&url='..URL.escape(keko2[2])..'&alias=')
+local AshtaR2 = {string.match(text, "^(اختصار) (.*)$")}
+local req = HTTP.request('http://caut-link.tk/api/?api=a591ca34f771237a385fd9ebadc13e0ec48490f7&url='..URL.escape(AshtaR2[2])..'&alias=')
 local data2 = JSON.decode(req)
 if not data2.shortenedUrl then  
 send(msg.chat_id_, msg.id_, 1, "حدث خطأ 🖲┇", 1, 'html')                 
 return false end	
-send(msg.chat_id_, msg.id_, 1, "🖲┇ الرابط ~⪼  {"..keko2[2]..'}\n\n 💡┇ الرابط المختصر  ~⪼ {'..data2.shortenedUrl..'}', 1, 'html')                 
+send(msg.chat_id_, msg.id_, 1, "🖲┇ الرابط ~⪼  {"..AshtaR2[2]..'}\n\n 💡┇ الرابط المختصر  ~⪼ {'..data2.shortenedUrl..'}', 1, 'html')                 
 end
 end
 end  -- end text 
 end
 return {
-    keko_tshake = keko_tshake,
+    AshtaR_GhareB = AshtaR_GhareB,
 }
 --[[
- BY : TshAkETEAM
- Channel Files : https://t.me/tshakeFiles
+ BY : AshtaRGhareB
 ]]
     
